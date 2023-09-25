d1da8d2c29b2fc1b890d0b0bf2721cbe973587f1
EunHop <thffoa99@gmail.com>
2023-09-25 23:32:40 +0900

    first commit to start project

---
+++ src/test/java/com/eunhop/tmdbmovieapp/TmdbMovieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eunhop.tmdbmovieapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmdbMovieAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
